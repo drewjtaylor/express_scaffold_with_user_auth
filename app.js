c5349da1cbac3d7350b1374441e3777c8d516cb4
Drew J Taylor <102109971+drewjtaylor@users.noreply.github.com>
2023-10-27 12:32:30 -0400

    Update app.js
    Security-prevent objects in query parameters

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ const app = express();
 // Set up Morgan middleware logging in dev mode
 app.use(morgan('dev'));
 
+// Only parse query parameters into strings, not objects (see https://masteringjs.io/tutorials/express/query-parameters and https://blog.websecurify.com/2014/08/hacking-nodejs-and-mongodb)
+app.set('query parser', 'simple');
+
 
 // Redirect non-secure requests to https
 app.all('*', (req, res, next) => {
